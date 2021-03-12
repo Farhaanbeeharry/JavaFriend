d49a8eb1ea1dac7668578fe47270112266f80e3e
Farhaan Beeharry <farhaanbeeharry.ms@gmail.com>
2021-03-12 19:42:09 +0400

    Added a structure and fun list

---
+++ assets/js/dictionary.js
@@ -0,0 +1,141 @@
+//Normal greetings
+var greetingList = [
+    "Hello, how can I help you?",
+    "Hi, how can I assist you today?",
+    "Greetings, how can I be of help to you?"
+]
+
+//Greetings with enquiry
+var greetingList2 = [
+    "Hello. I'm good. How can I assist you?",
+    "Hi, I'm fine. How can I be of help to you?",
+    "Greetings, doing good. How can I help you?"
+]
+
+//Java definition
+const javaDefinitionLink = "https://www.tutorialspoint.com/java/index.htm";
+var javaDefinition = [
+    'Java is a class-based and an Object Oriented Programming (OOP) language designed to have as few implementation dependencies as possible. You can read more by asking the system to open a Java definition page (e.g. "open Java definition")'
+]
+var openJavaDefinition = [
+    "Redirecting you to the Java definition page..."
+]
+
+//Java advantage
+const javaAdvantageLink = "https://www.tutorialspoint.com/java/index.htm";
+var advantageJava = [
+    "Java is object oriented, platform independent, simple, secure, architecture-neutral, portable and robust. You can learn more by asking to 'open advantages of Java'"
+]
+var advantageJavaRedirection = [
+    "Redirecting you to a page where you can learn more about Java..."
+]
+
+//Netbeans
+const netbeansLink = "https://www.netbeans.org/index.html";
+var netbeans = [
+    "NetBeans is a Java IDE that is free open-source. You can download the NetBeans by asking to 'open NetBeans page"
+]
+var netbeansRedirection = [
+    "Redirecting you to NetBeans page..."
+]
+
+//Environment setup
+const environmentSetupLink = "";
+var environmentSetup = []
+var environmentSetupRedirection = []
+
+//Loops
+
+//Basic Syntax
+
+//Object and Classes
+
+//Constructors
+
+//Basic Datatypes
+
+//Variable types
+
+//Modifier types
+
+//Basic operators
+
+//Decision making
+
+//Numbers
+
+//Characters
+
+//Strings
+
+//Arrays
+
+//Date and Time
+
+//Regular Expressions
+
+//Methods
+
+//File, input and output
+
+//Exceptions
+
+//Inner Classes
+
+//Hello World
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const loopLink = "https://www.tutorialspoint.com/java/java_loop_control.htm";
+var loopList = [
+    "Redirecting you to Java For-Loops page...",
+    "Opening a new page to teach you For-Loops in Java"
+]
+
+
+
+
+
+var helloWorld = [
+    "Redirecting you to the Hello World page where you can try your first Java program"
+]
+
+
+
+var environment = [
+    "Redirecting you to a page where you can learn how to set up your environment to program in Java"
+]
+
+var basicSyntax = [
+    "Redirecting you to the Java - Basic Syntax page"
+]
+
+var objectAndClasses = [
+    "Redirecting you to the Java - Object and Classes page"
+]
+
+//Unreadable list
+var unreadable = [
+    "Sorry, I couldn't catch that. Can you try again?",
+    "Apologies, I didn't understand. Can you please try again?"
+]
+
+//Fun List
+var koiMilGaya = [
+    "Mera dil gaya",
+    "Mil hi gaya",
+    "Haha that's funny you're singing xD"
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,3 @@
-var greetingList = [
-    "Hello, how can I help you?",
-    "Hi, how can I assist you today?",
-    "Greetings, how can I be of help to you?"
-]
-
-var greetingList2 = [
-    "Hello. I'm good. How can I assist you?",
-    "Hi, I'm fine. How can I be of help to you?",
-    "Greetings, doing good. How can I help you?"
-]
-
-var unreadable = [
-    "Sorry, I couldn't catch that. Can you try again?",
-    "Apologies, I didn't understand. Can you please try again?"
-]
-
-var forLoopList = [
-    "Redirecting you to Java For-Loops page...",
-    "Opening a new page to teach you For-Loops in Java"
-]
-
-var whileLoopsList = [
-    "Redirecting you to Java While-Loops page...",
-    "Opening a new page to teach you While-Loops in Java"
-]
-
-var javaDefinition = [
-    "Java is a class-based and an Object Oriented Programming (OOP) language designed to have as few implementation dependencies as possible. "
-]
-
 $(document).keypress('.manualInput', function(e) {
     if (e.which == 13) {
         e.preventDefault();
@@ -37,6 +6,8 @@ $(document).keypress('.manualInput', function(e) {
 });
 
 function speechBtnClicked() {
+    var speechBtn = document.getElementById('speechIcon');
+    speechBtn.classList = "fas fa-cog fa-spin fa-4x";
     recognition.start();
 }
 
@@ -56,45 +27,137 @@ recognition.addEventListener('result', (e) => {
     p.innerText = text;
     document.getElementById('texts').appendChild(p);
 
-
-
     if (e.results[0].isFinal) {
 
-        if (text.includes('how') && text.includes('are') && text.includes('you')) {
+        var speechBtn = document.getElementById('speechIcon');
+        speechBtn.classList = "fas fa-microphone fa-4x";
+
+        //Fun
+        if (text.includes("Koi Mil Gaya")) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * koiMilGaya.length);
+            p.innerText = koiMilGaya[random];
+            document.getElementById('texts').appendChild(p);
+        }
+        //Greetings with enquiry
+        else if (text.includes('how') && text.includes('are') && text.includes('you')) {
             p = document.createElement('p');
             const random = Math.floor(Math.random() * greetingList2.length);
             p.innerText = greetingList2[random];
             document.getElementById('texts').appendChild(p);
-        } else if (text.includes('hello') || text.includes('greetings')) {
+        }
+        //Normal greetings
+        else if (text.includes('hello') || text.includes('greetings')) {
             p = document.createElement('p');
             const random = Math.floor(Math.random() * greetingList.length);
             p.innerText = greetingList[random];
             document.getElementById('texts').appendChild(p);
-        } else if (text.includes('for loop') || text.includes('for loops')) {
+        }
+        //Java definition
+        else if (text.includes('open Java definition') || text.includes('open what is Java') || text.includes('open Java description')) {
             p = document.createElement('p');
-            const random = Math.floor(Math.random() * forLoopList.length);
-            p.innerText = forLoopList[random];
+            const random = Math.floor(Math.random() * openJavaDefinition.length);
+            p.innerText = openJavaDefinition[random];
             document.getElementById('texts').appendChild(p);
-        } else if (text.includes('while loop') || text.includes('while loops')) {
-            p = document.createElement('p');
-            const random = Math.floor(Math.random() * whileLoopsList.length);
-            p.innerText = whileLoopsList[random];
-            document.getElementById('texts').appendChild(p);
-        } else if (text.includes('what is Java') || text.includes('Java definition') || text.includes('Java description') || text.includes('define Java')) {
+            window.setTimeout(function() {
+                window.open(javaDefinitionLink);
+            }, 1000);
+        } else if (text.includes('what is Java') || text.includes('Java description') || text.includes('define Java')) {
             p = document.createElement('p');
             const random = Math.floor(Math.random() * javaDefinition.length);
             p.innerText = javaDefinition[random];
+            document.getElementById('texts').appendChild(p);
+        }
+        //Java advantage
+        else if (text.includes('open Java advantage') || text.includes("advantages of Java") || text.includes('open what are the advantages') || text.includes('open Java benefits')) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * advantageJavaRedirection.length);
+            p.innerText = advantageJavaRedirection[random];
+            document.getElementById('texts').appendChild(p);
+            window.setTimeout(function() {
+                window.open(javaAdvantageLink);
+            }, 1000);
+        } else if (text.includes('why learn Java') || text.includes('why use Java') || text.includes('advantage of Java') || text.includes('why do we use Java')) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * advantageJava.length);
+            p.innerText = advantageJava[random];
+            document.getElementById('texts').appendChild(p);
+        }
+        //Netbeans
+        else if (text.includes('open NetBeans page') | text.includes('open NetBeans website') || text.includes('more about NetBeans') || text.includes("NetBeans page")) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * netbeansRedirection.length);
+            p.innerText = netbeansRedirection[random];
+            document.getElementById('texts').appendChild(p);
+            window.setTimeout(function() {
+                window.open(netbeansLink);
+            }, 1000);
+        } else if (text.includes('what is NetBeans') || text.includes('definition of NetBeans') || text.includes('about NetBeans')) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * netbeans.length);
+            p.innerText = netbeans[random];
+            document.getElementById('texts').appendChild(p);
+        }
+
+
+
+
+
+
+        /*
+        
+        else if (text.includes('for loop') || text.includes('for loops')) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * loopList.length);
+            p.innerText = loopList[random];
+            document.getElementById('texts').appendChild(p);
+        }  else if (text.includes('why') && text.includes('use') && text.includes('Java')) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * advantageJava.length);
+            p.innerText = advantageJava[random];
             document.getElementById('texts').appendChild(p);
         } else if (text.includes('is Java OOP') || text.includes('is Java object oriented')) {
             p = document.createElement('p');
             p.innerText = "Yes, Java is an Object Oriented Programming (OOP) language";
             document.getElementById('texts').appendChild(p);
-        } else {
+        } else if (text.includes('hello world') || text.includes('basic tutorial') || text.includes('first tutorial') || text.includes('first program in Java')) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * helloWorld.length);
+            p.innerText = helloWorld[random];
+            document.getElementById('texts').appendChild(p);
+        } else if (text.includes('NetBeans')) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * netbeans.length);
+            p.innerText = netbeans[random];
+            document.getElementById('texts').appendChild(p);
+        } else if (text.includes('environment') && text.includes('setup')) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * environment.length);
+            p.innerText = environment[random];
+            document.getElementById('texts').appendChild(p);
+        } else if (text.includes('basic syntax') || text.includes('syntax') || text.includes('need to know')) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * basicSyntax.length);
+            p.innerText = basicSyntax[random];
+            document.getElementById('texts').appendChild(p);
+        } else if (text.includes('object') || text.includes('class') || text.includes('object and class')) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * objectAndClasses.length);
+            p.innerText = objectAndClasses[random];
+            document.getElementById('texts').appendChild(p);
+        } else if (text.includes('constructor')) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * basicSyntax.length);
+            p.innerText = basicSyntax[random];
+            document.getElementById('texts').appendChild(p);
+        } */
+        else {
             p = document.createElement('p');
             const random = Math.floor(Math.random() * unreadable.length);
             p.innerText = unreadable[random];
             document.getElementById('texts').appendChild(p);
         }
+
 
 
         p = document.createElement('p');
