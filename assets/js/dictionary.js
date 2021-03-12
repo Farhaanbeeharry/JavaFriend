477a87b724354d004ed040c53b0faf4a313504aa
Farhaan Beeharry <farhaanbeeharry.ms@gmail.com>
2021-03-12 22:31:51 +0400

    added more on dictionary

--- assets/js/dictionary.js
+++ assets/js/dictionary.js
@@ -33,99 +33,152 @@ var advantageJavaRedirection = [
 //Netbeans
 const netbeansLink = "https://www.netbeans.org/index.html";
 var netbeans = [
-    "NetBeans is a Java IDE that is free open-source. You can download the NetBeans by asking to 'open NetBeans page"
+    "NetBeans is a Java IDE that is free open-source. You can download the NetBeans by asking to 'open NetBeans page'"
 ]
 var netbeansRedirection = [
     "Redirecting you to NetBeans page..."
 ]
 
 //Environment setup
-const environmentSetupLink = "";
-var environmentSetup = []
-var environmentSetupRedirection = []
+const environmentSetupLink = "https://www.tutorialspoint.com/java/java_environment_setup.htm";
+var environmentSetup = [
+    "You need to have Java SE on your system to program in Java. One of the most popular IDE (editors) is NetBeans. You can read more about how to setup your environment by asking 'help me setup my environment'"
+]
+var environmentSetupRedirection = [
+    "Redirecting you to the environment setup page..."
+]
 
 //Loops
+const loopsLink = "https://www.tutorialspoint.com/java/java_loop_control.htm";
+var loopsList = [
+    "A loop statement allows us to execute a statement or group of statements multiple times. You can read more by asking 'read more on loops'"
+]
+var loopsRedirection = [
+    "Redirecting you to the Loops page of Java"
+]
 
 //Basic Syntax
+const basicSyntaxLink = "https://www.tutorialspoint.com/java/java_basic_syntax.htm";
+var basicSyntax = [
+    "The basic syntaxes of Java are the object (states and behaviours), class (template or blueprint), methods (behaviour) and instance variables (set of instance variables). To learn more, try 'open basic syntaxes'"
+]
+var basicSyntaxRedirection = [
+    "Redirecting you to the basic syntaxes of Java page..."
+]
 
 //Object and Classes
+const objectAndClassesLink = "https://www.tutorialspoint.com/java/java_object_classes.htm";
+var objectAndClasses = [
+    "Objects have states and behaviors. Example: A dog has states - color, name, breed as well as behaviors – wagging the tail, barking, eating. An object is an instance of a class. Class − A class can be defined as a template/blueprint that describes the behavior/state that the object of its type support. To learn more, say the command 'open object and classes'"
+]
+var objectAndClassesRedirection = [
+    "Redirecting you to the Object and Classes page"
+]
 
 //Constructors
+const constructorLink = "https://www.tutorialspoint.com/java/java_constructors.htm";
+var constructor = [
+    "A constructor initializes an object when it is created. It has the same name as its class and is syntactically similar to a method. However, constructors have no explicit return type. Try 'open constructor page' to learn more"
+]
+var constructorRedirection = [
+    "Redirecting you to the Constructor page..."
+]
 
 //Basic Datatypes
+const datatypeLink = "https://www.tutorialspoint.com/java/java_basic_datatypes.htm";
+var datatype = [
+    "Based on the data type of a variable, the operating system allocates memory and decides what can be stored in the reserved memory. Therefore, by assigning different data types to variables, you can store integers, decimals, or characters in these variables. There are two data types available in Java − Primitive Data Types and Reference/Object Data Types. To learn more, ask to 'open data type page'"
+]
+var datatypeRedirection = [
+    "Redirecting you to the data type page of Java..."
+]
 
 //Variable types
+const variableTypeLink = "https://www.tutorialspoint.com/java/java_variable_types.htm";
+var variableType = [
+    "A variable provides us with named storage that our programs can manipulate. Each variable in Java has a specific type, which determines the size and layout of the variable's memory; the range of values that can be stored within that memory; and the set of operations that can be applied to the variable. To learn more, say 'more on variable types'"
+]
+var variableTypeRedirection = [
+    "Redirecting you to the variable types page..."
+]
 
 //Modifier types
+const modifierTypeLink = "https://www.tutorialspoint.com/java/java_modifier_types.htm";
+var modifierType = [
+    "Modifiers are keywords that you add to those definitions to change their meanings. Say 'more on modifier types' to learn more."
+]
+var modifierTypeRedirection = [
+    "Redirecting you to the modifier types page of Java..."
+]
 
 //Basic operators
+const basicOperatorsLink = "https://www.tutorialspoint.com/java/java_basic_operators.htm";
+var basicOperators = [
+    "Java provides a rich set of operators to manipulate variables. To learn more, say 'more on basic operators'"
+]
+var basicOperatorsRedirection = [
+    "Redirecting you to basic operators page..."
+]
 
 //Decision making
+const decisionMakingLink = "https://www.tutorialspoint.com/java/java_decision_making.htm";
+var decisionMaking = []
+var decisionMakingRedirection = []
 
 //Numbers
+const numbersLink = "https://www.tutorialspoint.com/java/java_numbers.htm";
+var numbers = []
+var numbersRedirection = []
 
 //Characters
+const charactersLink = "https://www.tutorialspoint.com/java/java_characters.htm";
+var characters = []
+var charactersRedirection = []
 
 //Strings
+const stringLink = "https://www.tutorialspoint.com/java/java_strings.htm";
+var string = []
+var stringRedirection = []
 
 //Arrays
+const arraysLink = "https://www.tutorialspoint.com/java/java_arrays.htm";
+var arrays = []
+var arraysRedirection = []
 
 //Date and Time
+const dateAndTimeLink = "https://www.tutorialspoint.com/java/java_date_time.htm";
+var dateAndTime = []
+var dateAndTimeRedirection = []
 
 //Regular Expressions
+const regexLink = "https://www.tutorialspoint.com/java/java_regular_expressions.htm";
+var regex = []
+var regexRedirection = []
 
 //Methods
+const methodsLink = "https://www.tutorialspoint.com/java/java_methods.htm";
+var methods = []
+var methodsRedirection = []
 
 //File, input and output
+const fileIOLink = "https://www.tutorialspoint.com/java/java_files_io.htm";
+var fileIO = []
+var fileIORedirection = []
 
 //Exceptions
+const exceptionLink = "https://www.tutorialspoint.com/java/java_exceptions.htm";
+var exception = []
+var exceptionRedirection = []
 
 //Inner Classes
+const innerClassLink = "https://www.tutorialspoint.com/java/java_innerclasses.htm";
+var innerClass = []
+var innerClassRedirection = []
 
 //Hello World
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const loopLink = "https://www.tutorialspoint.com/java/java_loop_control.htm";
-var loopList = [
-    "Redirecting you to Java For-Loops page...",
-    "Opening a new page to teach you For-Loops in Java"
-]
-
-
-
-
-
-var helloWorld = [
-    "Redirecting you to the Hello World page where you can try your first Java program"
-]
-
-
-
-var environment = [
-    "Redirecting you to a page where you can learn how to set up your environment to program in Java"
-]
-
-var basicSyntax = [
-    "Redirecting you to the Java - Basic Syntax page"
-]
-
-var objectAndClasses = [
-    "Redirecting you to the Java - Object and Classes page"
-]
+const helloWorldLink = "https://www.tutorialspoint.com/java/index.htm";
+var helloWorld = []
+var helloWorldRedirection = []
 
 //Unreadable list
 var unreadable = [
--- assets/js/script.js
+++ assets/js/script.js
@@ -39,6 +39,12 @@ recognition.addEventListener('result', (e) => {
             p.innerText = koiMilGaya[random];
             document.getElementById('texts').appendChild(p);
         }
+        //Questions
+        else if (text.includes('is Java OOP') || text.includes('is Java object oriented')) {
+            p = document.createElement('p');
+            p.innerText = "Yes, Java is an Object Oriented Programming (OOP) language";
+            document.getElementById('texts').appendChild(p);
+        }
         //Greetings with enquiry
         else if (text.includes('how') && text.includes('are') && text.includes('you')) {
             p = document.createElement('p');
@@ -62,7 +68,12 @@ recognition.addEventListener('result', (e) => {
             window.setTimeout(function() {
                 window.open(javaDefinitionLink);
             }, 1000);
-        } else if (text.includes('what is Java') || text.includes('Java description') || text.includes('define Java')) {
+        } else if (text.includes('what') && text.includes('is') && text.includes('Java')) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * javaDefinition.length);
+            p.innerText = javaDefinition[random];
+            document.getElementById('texts').appendChild(p);
+        } else if (text.includes('what is Java') || text.includes('Java description') || text.includes('define Java') || text === "Java") {
             p = document.createElement('p');
             const random = Math.floor(Math.random() * javaDefinition.length);
             p.innerText = javaDefinition[random];
@@ -84,7 +95,7 @@ recognition.addEventListener('result', (e) => {
             document.getElementById('texts').appendChild(p);
         }
         //Netbeans
-        else if (text.includes('open NetBeans page') | text.includes('open NetBeans website') || text.includes('more about NetBeans') || text.includes("NetBeans page")) {
+        else if (text.includes('open NetBeans page') || text.includes('open NetBeans website') || text.includes('more about NetBeans') || text.includes("NetBeans page")) {
             p = document.createElement('p');
             const random = Math.floor(Math.random() * netbeansRedirection.length);
             p.innerText = netbeansRedirection[random];
@@ -98,59 +109,151 @@ recognition.addEventListener('result', (e) => {
             p.innerText = netbeans[random];
             document.getElementById('texts').appendChild(p);
         }
-
-
-
-
-
-
-        /*
-        
-        else if (text.includes('for loop') || text.includes('for loops')) {
+        //environment setup
+        else if (text.includes('help me set up my environment') || text.includes('help me setup my environment') || text.includes('how to setup environment') || text.includes('more on environment setup')) {
             p = document.createElement('p');
-            const random = Math.floor(Math.random() * loopList.length);
-            p.innerText = loopList[random];
+            const random = Math.floor(Math.random() * environmentSetupRedirection.length);
+            p.innerText = environmentSetupRedirection[random];
             document.getElementById('texts').appendChild(p);
-        }  else if (text.includes('why') && text.includes('use') && text.includes('Java')) {
+            window.setTimeout(function() {
+                window.open(environmentSetupLink);
+            }, 1000);
+        } else if (text.includes('environment') || text.includes('brief on environment') || text.includes('setup to use Java')) {
             p = document.createElement('p');
-            const random = Math.floor(Math.random() * advantageJava.length);
-            p.innerText = advantageJava[random];
+            const random = Math.floor(Math.random() * environmentSetup.length);
+            p.innerText = environmentSetup[random];
             document.getElementById('texts').appendChild(p);
-        } else if (text.includes('is Java OOP') || text.includes('is Java object oriented')) {
+        }
+        //Loops
+        else if (text.includes('read more on loop') || text.includes('more on loop') || text.includes('open loops page') || text.includes('open loop page') || text.includes('more about loop')) {
             p = document.createElement('p');
-            p.innerText = "Yes, Java is an Object Oriented Programming (OOP) language";
+            const random = Math.floor(Math.random() * loopsRedirection.length);
+            p.innerText = loopsRedirection[random];
             document.getElementById('texts').appendChild(p);
-        } else if (text.includes('hello world') || text.includes('basic tutorial') || text.includes('first tutorial') || text.includes('first program in Java')) {
+            window.setTimeout(function() {
+                window.open(loopsLink);
+            }, 1000);
+        } else if (text.includes('what is a loop') || text.includes('what is loop') || text.includes('define loop') || text.includes('definition of loop')) {
             p = document.createElement('p');
-            const random = Math.floor(Math.random() * helloWorld.length);
-            p.innerText = helloWorld[random];
+            const random = Math.floor(Math.random() * loopsList.length);
+            p.innerText = loopsList[random];
             document.getElementById('texts').appendChild(p);
-        } else if (text.includes('NetBeans')) {
+        }
+        //Basic syntaxes
+        else if (text.includes('open basic syntax') || text.includes('more on basic syntax') || text.includes('basic syntaxes page') || text.includes('basic syntax page')) {
             p = document.createElement('p');
-            const random = Math.floor(Math.random() * netbeans.length);
-            p.innerText = netbeans[random];
+            const random = Math.floor(Math.random() * basicSyntaxRedirection.length);
+            p.innerText = basicSyntaxRedirection[random];
             document.getElementById('texts').appendChild(p);
-        } else if (text.includes('environment') && text.includes('setup')) {
-            p = document.createElement('p');
-            const random = Math.floor(Math.random() * environment.length);
-            p.innerText = environment[random];
-            document.getElementById('texts').appendChild(p);
-        } else if (text.includes('basic syntax') || text.includes('syntax') || text.includes('need to know')) {
+            window.setTimeout(function() {
+                window.open(basicSyntaxLink);
+            }, 1000);
+        } else if (text.includes('what is a basic syntax') || text.includes('what are basic syntaxes') || text.includes('basic syntax') || text.includes('definition of basic syntax') || text.includes("define basic syntax")) {
             p = document.createElement('p');
             const random = Math.floor(Math.random() * basicSyntax.length);
             p.innerText = basicSyntax[random];
             document.getElementById('texts').appendChild(p);
-        } else if (text.includes('object') || text.includes('class') || text.includes('object and class')) {
+        }
+        //Object and Classes
+        else if (text.includes('open object and class') || text.includes('more on object and class') || text.includes('go to object and class') || text.includes('object and classes page') || text.includes('object and class page')) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * objectAndClassesRedirection.length);
+            p.innerText = objectAndClassesRedirection[random];
+            document.getElementById('texts').appendChild(p);
+            window.setTimeout(function() {
+                window.open(objectAndClassesLink);
+            }, 1000);
+        } else if (text.includes('what is object and class') || text.includes('what are object and class') || text.includes('object and class') || text.includes('definition of object and class') || text.includes("define object and class")) {
             p = document.createElement('p');
             const random = Math.floor(Math.random() * objectAndClasses.length);
             p.innerText = objectAndClasses[random];
             document.getElementById('texts').appendChild(p);
-        } else if (text.includes('constructor')) {
+        }
+        //Constructors
+        else if (text.includes('open constructor') || text.includes('more on constructor') || text.includes('go to constructor') || text.includes('constructor page') || text.includes('constructors page')) {
             p = document.createElement('p');
-            const random = Math.floor(Math.random() * basicSyntax.length);
-            p.innerText = basicSyntax[random];
+            const random = Math.floor(Math.random() * constructorRedirection.length);
+            p.innerText = constructorRedirection[random];
             document.getElementById('texts').appendChild(p);
-        } */
+            window.setTimeout(function() {
+                window.open(constructorLink);
+            }, 1000);
+        } else if (text.includes('what is constructor') || text.includes('what are constructors') || text.includes('constructor') || text.includes('definition of constructor') || text.includes("define constructor")) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * constructor.length);
+            p.innerText = constructor[random];
+            document.getElementById('texts').appendChild(p);
+        }
+        //Data types
+        else if (text.includes('open data type') || text.includes('more on data type') || text.includes('go to data type') || text.includes('data type page') || text.includes('data types page')) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * datatypeRedirection.length);
+            p.innerText = datatypeRedirection[random];
+            document.getElementById('texts').appendChild(p);
+            window.setTimeout(function() {
+                window.open(datatypeLink);
+            }, 1000);
+        } else if (text.includes('what is data type') || text.includes('what are data type') || text.includes('data type') || text.includes('definition of data type') || text.includes("define data type")) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * datatype.length);
+            p.innerText = datatype[random];
+            document.getElementById('texts').appendChild(p);
+        }
+        //variable types
+        else if (text.includes('open variable type') || text.includes('more on variable type') || text.includes('go to variable type') || text.includes('variable type page') || text.includes('variable types page')) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * variableTypeRedirection.length);
+            p.innerText = variableTypeRedirection[random];
+            document.getElementById('texts').appendChild(p);
+            window.setTimeout(function() {
+                window.open(variableTypeLink);
+            }, 1000);
+        } else if (text.includes('what is variable type') || text.includes('what are variable type') || text.includes('variable type') || text.includes('definition of variable type') || text.includes("define variable type")) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * variableType.length);
+            p.innerText = variableType[random];
+            document.getElementById('texts').appendChild(p);
+        }
+        //Modifier types
+        else if (text.includes('open modifier type') || text.includes('more on modifier type') || text.includes('go to modifier type') || text.includes('modifier type page') || text.includes('modifier types page')) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * modifierTypeRedirection.length);
+            p.innerText = modifierTypeRedirection[random];
+            document.getElementById('texts').appendChild(p);
+            window.setTimeout(function() {
+                window.open(modifierTypeLink);
+            }, 1000);
+        } else if (text.includes('what is modifier type') || text.includes('what are modifier type') || text.includes('modifier type') || text.includes('definition of modifier type') || text.includes("define modifier type")) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * modifierType.length);
+            p.innerText = modifierType[random];
+            document.getElementById('texts').appendChild(p);
+        }
+        //Basic Operators
+        else if (text.includes('open basic operator') || text.includes('more on basic operator') || text.includes('go to basic operator') || text.includes('basic operator page') || text.includes('basic operators page')) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * basicOperatorsRedirection.length);
+            p.innerText = basicOperatorsRedirection[random];
+            document.getElementById('texts').appendChild(p);
+            window.setTimeout(function() {
+                window.open(basicOperatorsLink);
+            }, 1000);
+        } else if (text.includes('what is basic operator') || text.includes('what are basic operator') || text.includes('basic operator') || text.includes('definition of basic operator') || text.includes("define basic operator")) {
+            p = document.createElement('p');
+            const random = Math.floor(Math.random() * basicOperators.length);
+            p.innerText = basicOperators[random];
+            document.getElementById('texts').appendChild(p);
+        }
+
+
+
+
+
+
+
+
+
+        //unreadable
         else {
             p = document.createElement('p');
             const random = Math.floor(Math.random() * unreadable.length);
