662136c698e4e0e54591e01ada63384551e27e27
Farhaan Beeharry <farhaanbeeharry.ms@gmail.com>
2021-03-15 18:31:02 +0400

    added no inspect element from keyboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,25 @@ window.onload = function() {
     playVoice("Welcome to this application. If you're asking me to open an external webpage or using voice commands for the first time, it will be blocked by your browser. Kindly allow popups when asked and try again. How can I help you?");
 }
 
+document.onkeydown = function(e) {
+    if (event.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
+
+
 $(document).keypress('.manualInput', function(e) {
     if (e.which == 13) {
         e.preventDefault();
